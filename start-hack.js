3b8f17e779e37a2e9cbb4e2b2dc220a54d8ad2a4
Leone Varando <lvarando@gmail.com>
2024-01-10 01:01:37 -0600

    Added hack start for level deeper in servers

--- start-hack.js
+++ start-hack.js
@@ -4,6 +4,10 @@ export async function main(ns) {
   for (const server of serverList) {
     ns.exec('hack.js', server);
 
-    
+    const subServerList = ns.scan(server);
+
+    subServerList.forEach(subServer => {
+      ns.exec('hack.js', subServer);
+    });
   }
 }
