6df4c1aa3a3fae3b98fb0f0912f019d90b38b964
Leone Varando <lvarando@gmail.com>
2024-01-10 03:37:14 -0600

    Added variable multithreading based on ram in current server

--- start-hack.js
+++ start-hack.js
@@ -1,15 +1,23 @@
+/** @param {NS} ns */
 export async function main(ns) {
   const serverList = ns.scan(ns.getHostname());
 
   for (const server of serverList) {
-    ns.exec('hack.js', server, 3);
+    try {
+      let t = (Math.floor((ns.getServerMaxRam(server) - ns.getServerUsedRam(server)) / ns.getScriptRam('hack.js', server)));
 
-    const subServerList = ns.scan(server);
+      ns.exec('hack.js', server, t);
 
-    if (subServerList.includes('home')) subServerList.splice(0, 1);
+      const subServerList = ns.scan(server);
 
-    subServerList.forEach(subServer => {
-      ns.exec('hack.js', subServer, 3);
-    });
+      if (subServerList.includes('home')) subServerList.splice(0, 1);
+
+      subServerList.forEach(subServer => {
+        let t = (Math.floor((ns.getServerMaxRam(subServer) - ns.getServerUsedRam(subServer)) / ns.getScriptRam('hack.js', subServer)));
+        ns.exec('hack.js', subServer, t);
+      });
+    } catch (e) {
+      ns.tprint(e.message);
+    }
   }
 }
