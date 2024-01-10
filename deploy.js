029d70b740458c6c0546ba5675ad1b44a83f71d4
Leone Varando <lvarando@gmail.com>
2024-01-10 00:55:05 -0600

    Expanded deploy to include servers 2 layers deep

--- deploy.js
+++ deploy.js
@@ -3,6 +3,17 @@ export async function main(ns) {
   const serverList = ns.scan(ns.getHostname());
   
   for (const server of serverList) {
-    ns.scp("hack.js", server, "home");
+    if (!ns.fileExists('hack.js', server)) ns.scp("hack.js", server, "home");
+    else {
+      const subServerList = ns.scan(server);
+
+      if (subServerList.includes('home')) subServerList.splice(0, 1);
+
+      subServerList.forEach((subServer) => {
+        if (!ns.fileExists('hack.js', subServer)) {
+          ns.scp("hack.js", subServer, "home");
+        }
+      })
+    }
   }
 }
